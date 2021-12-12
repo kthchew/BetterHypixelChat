708b17c0c68e8e456a1e89437cb7a29e9199422b
Kenneth Chew <kenneth.c0@protonmail.com>
2021-12-11 20:51:52 -0500

    Fix regex

--- src/main/java/io/github/kthchew/betterhypixelchat/ChatListener.java
+++ src/main/java/io/github/kthchew/betterhypixelchat/ChatListener.java
@@ -2,6 +2,7 @@ package io.github.kthchew.betterhypixelchat;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -11,9 +12,13 @@ public class ChatListener {
         String message = event.message.getFormattedText();
 
         // Fix non chat
-        // Example: 'Player123: hello'
-        if (message.matches("^§7([^\\s]+): .*")) {
-            message = message.replaceFirst(":", ":§r");
+        // Example: '§r§7Player123§7§r§7: hello§r'
+        // But in SkyBlock, chat does not start with a RESET formatter: '§7Player123§7§r§7: hello§r'
+        String nonRegex = "^" + EnumChatFormatting.RESET + EnumChatFormatting.GRAY + ".*" + EnumChatFormatting.GRAY + EnumChatFormatting.RESET + EnumChatFormatting.GRAY + ": .*" + EnumChatFormatting.RESET;
+        String skyBlockNonRegex = "^" + EnumChatFormatting.GRAY + ".*" + EnumChatFormatting.GRAY + EnumChatFormatting.RESET + EnumChatFormatting.GRAY + ": .*" + EnumChatFormatting.RESET;
+
+        if (message.matches(nonRegex) || message.matches(skyBlockNonRegex)) {
+            message = message.replaceFirst(":", ":" + EnumChatFormatting.RESET);
             event.setCanceled(true);
             Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(message));
             return;
@@ -21,7 +26,8 @@ public class ChatListener {
 
         // Fix PM chat
         // Example: 'From [OWNER] hypixel: hello'
-        if (message.matches("^§dFrom .*§7: .*")) {
+        String pmRegex = EnumChatFormatting.LIGHT_PURPLE + "From .*" + EnumChatFormatting.GRAY + ": .*";
+        if (message.matches(pmRegex)) {
             message = message.replaceFirst(":", ":§r");
             event.setCanceled(true);
             Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(message));
